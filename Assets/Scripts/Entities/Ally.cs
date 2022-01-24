0358007041d67e5817afd06053f4860d2e244050
morganasirenius <morganasirenius@gmail.com>
2022-01-23 21:50:48 -0800

    shield when ally is being rescued

--- Assets/Scripts/Entities/Ally.cs
+++ Assets/Scripts/Entities/Ally.cs
@@ -7,14 +7,23 @@ public class Ally : Entity
     private float currentTime; // The current time a food is being transformed
     [SerializeField] private float maxTime = 1;
     private bool rescued = false;
-    [SerializeField] private Transform target;
+    private Transform target;
 
    [SerializeField] private float rescueSpeed;
-   [SerializeField] private GameObject shield;
+   [SerializeField] private Material shield;
+
+    private Renderer _renderer;
+    private MaterialPropertyBlock _propBlock;
+   private float shieldStep;
+   public GameObject TheShield;
     // Start is called before the first frame update
     void Start()
     {
-        shield.SetActive(false);
+        _propBlock = new MaterialPropertyBlock();
+        _renderer = TheShield.GetComponent<Renderer>();
+        Debug.Log(_propBlock);
+        Debug.Log(_renderer);
+        target = PlayerController.Instance.transform;
     }
 
     // Update is called once per frame
@@ -29,12 +38,15 @@ public class Ally : Entity
     public bool Rescue()
     {
         currentTime += Time.deltaTime;
-        int timeSeconds = (int)currentTime;
-        if (timeSeconds == maxTime)
+        float percentageTime = 1 - (currentTime / maxTime);
+        _renderer.GetPropertyBlock(_propBlock);
+        _propBlock.SetFloat("_EdgeDisolve", percentageTime);
+        _renderer.SetPropertyBlock(_propBlock);
+
+        if (currentTime >= maxTime)
         {
             Debug.Log("You rescued them yay");
             rescued = true;
-            shield.SetActive(true);
         }
         return rescued;
     }
