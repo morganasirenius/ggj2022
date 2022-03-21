99ea4c4e9ae2f0a532529677fb5713aa65f1a943
dzhan008 <9047851+dzhan008@users.noreply.github.com>
2022-03-20 21:50:28 -0700

    Fix entity health bug

--- Assets/Scripts/Entities/Entity.cs
+++ Assets/Scripts/Entities/Entity.cs
@@ -15,30 +15,15 @@ public class Entity : MonoBehaviour
 
     protected int currentHealth;
 
-    // private bool hasBeenInvisible;
-
     public virtual void OnSpawn() { return; }
     public virtual void Attack() { return; }
     public virtual void Move() { return; }
     public virtual void OnDeath() { return; }
-
     public virtual void OnReset() { return; }
 
-    protected virtual void Update()
+    void Start()
     {
-        /*
-        // If the object is now seen in the camera, it has been invisible before (when it was spawning)
-        if (GetComponent<Renderer>().isVisible && hasBeenInvisible == false)
-        {
-            Debug.Log("Object spawning in the screen!");
-            hasBeenInvisible = true;
-        }
-        else if (hasBeenInvisible == true) // If has been invisble already and is out of the screen, disable the gameobject
-        {
-            Debug.Log("Object off the screen!");
-            gameObject.SetActive(false);
-            hasBeenInvisible = false;
-        }*/
+        currentHealth = health;
     }
 
     protected virtual void OnDisable()
@@ -49,15 +34,14 @@ public class Entity : MonoBehaviour
 
     protected virtual void OnBecameInvisible()
     {
-        Debug.Log("Entity invisible: " + gameObject.name);
         OnReset();
         ResetEntity();
     }
 
     protected void ResetEntity()
     {
-        gameObject.SetActive(false);
         currentHealth = health;
+        gameObject.SetActive(false);
     }
 
     protected void AddScore()
