1c7b411ea72137453c998cdef732833e79f61518
Xolti <9047851+dzhan008@users.noreply.github.com>
2022-05-08 23:31:41 -0700

    Gachapon rolls (#36)
    * Add roll currency
    
    * Update MainMenu + move opacity conditional

--- Assets/Scripts/Gachapon/Gachapon.cs
+++ Assets/Scripts/Gachapon/Gachapon.cs
@@ -12,21 +12,48 @@ public class Gachapon : MonoBehaviour
     [SerializeField]
     private TMP_Text GachaText;
 
+    [SerializeField]
+    private TMP_Text RollsText;
+
+    void OnEnable()
+    {
+        GachaText.text = "Roll to see what you get!";
+        RollsText.text = PlayerData.Instance.Rolls.ToString();
+        // Set sprite to be invisible
+        GachaImage.color = new Color32(255, 255, 255, 0);
+        Debug.Log(string.Format("You have {0} rolls!", PlayerData.Instance.Rolls.ToString()));
+    }
+
     public void Roll()
     {
-        Globals.GachaponRarities rarity = DetermineRarity();
-        AnimalData[] dataArray = ResourceManager.Instance.AnimalDataDictionary[rarity.ToString()];
-        AnimalData animal = dataArray[Random.Range(0, dataArray.Length)];
-        GachaImage.sprite = animal.sprite;
-        if (PlayerData.Instance.acquiredAnimals.ContainsKey(animal))
+
+
+        if (PlayerData.Instance.Rolls > 0)
         {
-            PlayerData.Instance.acquiredAnimals[animal] += 1;
+            // Set sprite to be visible if it was previously invisible
+            if (GachaImage.color.a == 0)
+            {
+                GachaImage.color = new Color32(255, 255, 255, 255);
+            }
+
+            Globals.GachaponRarities rarity = DetermineRarity();
+            AnimalData[] dataArray = ResourceManager.Instance.AnimalDataDictionary[rarity.ToString()];
+            AnimalData animal = dataArray[Random.Range(0, dataArray.Length)];
+            GachaImage.sprite = animal.sprite;
+            if (PlayerData.Instance.acquiredAnimals.ContainsKey(animal))
+            {
+                PlayerData.Instance.acquiredAnimals[animal] += 1;
+            }
+            else
+            {
+                PlayerData.Instance.acquiredAnimals[animal] = 1;
+            }
+            GachaText.text = string.Format("{0}! \n You got {1}!", animal.rarity.ToString(), animal.animalName);
+
+            PlayerData.Instance.Rolls--;
+            RollsText.text = PlayerData.Instance.Rolls.ToString();
+            Debug.Log(string.Format("Rolls remaining: {0}", PlayerData.Instance.Rolls.ToString()));
         }
-        else
-        {
-            PlayerData.Instance.acquiredAnimals[animal] = 1;
-        }
-        GachaText.text = string.Format("{0}! \n You got {1}!", animal.rarity.ToString(), animal.animalName);
     }
 
     public Globals.GachaponRarities DetermineRarity()
