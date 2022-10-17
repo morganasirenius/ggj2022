c29e24c66176be13dbb0aa54f2ed06624f1fc0ce
Morgana <79112936+morganasirenius@users.noreply.github.com>
2022-10-16 22:00:45 -0700

    Fix background loop (#60)
    The original image was not in same multiple of 1020. It was a few pixel short so thats why there was a gap.

--- Assets/Scripts/Basic/Scroller.cs
+++ Assets/Scripts/Basic/Scroller.cs
@@ -9,6 +9,6 @@ public class Scroller : MonoBehaviour {
 
     void Update()
     {
-        _img.uvRect = new Rect(_img.uvRect.position + new Vector2(_x,_y) * Time.deltaTime,new Vector2(_img.uvRect.size.x,_img.uvRect.size.y));
+        _img.uvRect = new Rect(_img.uvRect.position + new Vector2(_x,_y) * Time.deltaTime, _img.uvRect.size);
     }
 }
